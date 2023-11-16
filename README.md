e38354a4ef5c286d4b70f5e322a256a0476594b9
RVanVlietII <rcvanvlietii@gmail.com>
2023-11-16 13:54:47 -0500

    modified webpack config to handle build and added server.js file to connect backend

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # PWA-Text-Editor
+
+## Description
+
+This app is a text editer that allows you to edit text online or offline. Includes a service worker for offline capabilities. 
+
--- client/service-worker.js
+++ client/service-worker.js
@@ -1,9 +1,56 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+// const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+// const { CacheFirst } = require('workbox-strategies');
+// const { registerRoute } = require('workbox-routing');
+// const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+// const { ExpirationPlugin } = require('workbox-expiration');
+// const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+
+// precacheAndRoute(self.__WB_MANIFEST);
+
+// const pageCache = new CacheFirst({
+//   cacheName: 'page-cache',
+//   plugins: [
+//     new CacheableResponsePlugin({
+//       statuses: [0, 200],
+//     }),
+//     new ExpirationPlugin({
+//       maxAgeSeconds: 30 * 24 * 60 * 60,
+//     }),
+//   ],
+// });
+
+// warmStrategyCache({
+//   urls: ['/index.html', '/'],
+//   strategy: pageCache,
+// });
+
+// registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+
+// registerRoute(
+// ({request}) =>["style","script","worker"].includes(request.destination),
+// new StaleWhileRevalidate({
+//   cacheName: "asset-cache",
+//     plugins: [  
+
+//       new CacheableResponsePlugin({
+//         statuses: [0, 200],
+        
+//       }),
+//     ],
+//   })
+
+
+
+
+
+// );
+
+import { precacheAndRoute } from 'workbox-precaching';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { registerRoute } from 'workbox-routing';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -19,29 +66,16 @@ const pageCache = new CacheFirst({
   ],
 });
 
-warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
-});
-
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-
 registerRoute(
-({request}) =>["style","script","worker"].includes(request.destination),
-new StaleWhileRevalidate({
-  cacheName: "asset-cache",
-    plugins: [  
-
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: "asset-cache",
+    plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
-        
       }),
     ],
   })
-
-
-
-
-
 );
